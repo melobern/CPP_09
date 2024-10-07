80a55082658f3ec9d3c8a2011d40e219fc2c1a14
melodie <melodie.bernard123@yahoo.fr>
2024-10-07 21:42:13 +0200

    calendar normed

--- ex00/includes/Calendar.hpp
+++ ex00/includes/Calendar.hpp
@@ -6,7 +6,7 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 09:07:06 by mbernard          #+#    #+#             */
-/*   Updated: 2024/10/07 21:39:06 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/10/07 21:41:50 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,27 +25,27 @@
 
 class Calendar {
  private:
-   int                       _days_in_month[13];
+  int                       _days_in_month[13];
 
-   Calendar &operator=(const Calendar &src);
+  Calendar &operator=(const Calendar &src);
 
   bool                      isLeap(const int year);
 
  public:
-   Calendar(void);
-   Calendar(const Calendar &src);
-   ~Calendar(void);
+  Calendar(void);
+  Calendar(const Calendar &src);
+  ~Calendar(void);
 
-void add_days_to_date(int mm, int dd, int yy, int days_left_to_add);
-bool dateIsWrong(const int dd, const int mm, const int yy);
-void printLeapYears(const int min, const int max);
-   class wrongDateException : public std::exception {
+  void add_days_to_date(int mm, int dd, int yy, int days_left_to_add);
+  bool dateIsWrong(const int dd, const int mm, const int yy);
+  void printLeapYears(const int min, const int max);
+  class wrongDateException : public std::exception {
    public:
       virtual const char* what() const throw();
-   };
-   class NotExistingYearException : public std::exception {
+  };
+  class NotExistingYearException : public std::exception {
    public:
       virtual const char* what() const throw();
-   };
+  };
 };
 #endif  // EX00_INCLUDES_CALENDAR_HPP_
