e876c378c29798c83c07cad29ec9eac5ff7b3df7
melodie <melodie.bernard123@yahoo.fr>
2024-10-09 21:51:57 +0200

    cpplint Colors.hpp

--- ex00/includes/Colors.hpp
+++ ex00/includes/Colors.hpp
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _COLORS_HPP_
-#define _COLORS_HPP_
+#ifndef EX00_INCLUDES_COLORS_HPP_
+#define EX00_INCLUDES_COLORS_HPP_
 
 #define RED "\033[31m"
 #define GREEN "\033[0;32m"
@@ -22,4 +22,4 @@
 #define BLUE "\033[94m"
 #define RESET "\033[0m"
 
-#endif  //  _COLORS_HPP_
+#endif  //  EX00_INCLUDES_COLORS_HPP_
